9bca257e1ef9b7b2e1514ac6daef6e935b50aad0
Marcel Araujo <marcel.araujo@inphonex.com>
2015-03-16 10:49:05 -0300

    Remove test file

--- test.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var assert = require('assert');
-var path = require('path');
-var File = require('./lib/file.js');
-
-var t = new File('./test/fixtures/usemin.html');
-
-console.log( t );
