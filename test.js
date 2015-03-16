3159a5707a6f24ab34c418fdff25cf6e6ef325ee
Marcel Araujo <marcel.araujo@inphonex.com>
2015-03-15 22:32:59 -0300

    Using AST html parser

---
+++ test.js
@@ -0,0 +1,8 @@
+'use strict';
+var assert = require('assert');
+var path = require('path');
+var File = require('./lib/file.js');
+
+var t = new File('./test/fixtures/usemin.html');
+
+//console.log( t );
